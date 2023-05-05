54171bdecff6fe01d4d86e47b0bc9df7f2c15104
666 MEMENTO <queen1448@nate.com>
2023-05-05 15:47:39 +0900

    save

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,7 +1,12 @@
 const TEXT = document.getElementById("intro_text");
+const MOON = document.getElementById("moon");
+
+
 const TEXT_HEIGHT = TEXT.getBoundingClientRect().height;
+const MOON_HEIGHT = MOON.getBoundingClientRect().height;
 
 console.log(TEXT_HEIGHT);
+console.log(MOON_HEIGHT);
 
 console.log(TEXT);
 
@@ -12,6 +17,16 @@ window.addEventListener("mousewheel", ()=>{
     TEXT.style.backgroundColor = "none";
   } else {
     TEXT.style.opacity = 1;
-    TEXT.style.transition = "1.5s";
+    TEXT.style.transition = "1.7s";
+  }
+});
+
+window.addEventListener("mousewheel", ()=>{
+  if(window.scrollY > MOON_HEIGHT) {
+    MOON.style.opacity = 0.01;
+    MOON.style.backgroundColor = "none";
+  } else {
+    MOON.style.opacity = 1;
+    MOON.style.transition = "1.7s";
   }
 });
